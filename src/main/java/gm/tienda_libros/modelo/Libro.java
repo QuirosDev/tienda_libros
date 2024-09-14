84fb52aa28e7d7c5724a1099debbd38b2a4fd003
Julian Esteban Quiros Cano <90948626+QuirosDev@users.noreply.github.com>
2024-09-14 13:13:36 -0500

    Initial commit

---
+++ src/main/java/gm/tienda_libros/modelo/Libro.java
@@ -0,0 +1,70 @@
+package gm.tienda_libros.modelo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idLibro;
+    String nombreLibro;
+    String autor;
+    Double precio;
+    Integer existencias;
+    public Libro() {
+    }
+
+    public Libro(Integer idLibro, String nombreLibro, String autor, Double precio, Integer existencias) {
+        this.idLibro = idLibro;
+        this.nombreLibro = nombreLibro;
+        this.autor = autor;
+        this.precio = precio;
+        this.existencias = existencias;
+    }
+
+    public Integer getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(Integer idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getNombreLibro() {
+        return nombreLibro;
+    }
+
+    public void setNombreLibro(String nombreLibro) {
+        this.nombreLibro = nombreLibro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getExistencias() {
+        return existencias;
+    }
+
+    public void setExistencias(Integer existencias) {
+        this.existencias = existencias;
+    }
+
+
+}
